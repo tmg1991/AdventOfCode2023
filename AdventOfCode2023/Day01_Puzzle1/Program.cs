10702f3184815778c89e73dbb2fcb2b23a6fe6cc
gabesztm <gabesz.tm@gmail.com>
2023-12-01 12:25:26 +0100

    Day01_Puzzle1

---
+++ AdventOfCode2023/Day01_Puzzle1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var inputPath = "input.txt";
+string[] lines = File.ReadAllLines(inputPath);
+
+int sumOfCalibrationValues = 0;
+
+foreach (string line in lines)
+{
+    var characterLine = line.ToCharArray();
+    if (!characterLine.Any(_ => Char.IsDigit(_)))
+    {
+        continue;
+    }
+    var firstDigitChar = characterLine.First(_ => Char.IsDigit(_));
+    int firstDigit = int.Parse(firstDigitChar.ToString());
+    var lastDigitChar = characterLine.Last(_ => Char.IsDigit(_));
+    int lastDigit = int.Parse(lastDigitChar.ToString());
+
+    string combinedChars = $"{firstDigitChar}{lastDigitChar}";
+
+    var combinecNumber = int.Parse(combinedChars);
+
+    sumOfCalibrationValues += combinecNumber;
+
+
+}
+
+Console.WriteLine(sumOfCalibrationValues);
